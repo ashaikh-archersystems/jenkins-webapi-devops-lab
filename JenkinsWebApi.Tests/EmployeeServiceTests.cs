3ed20f4511a578508b789b41d3e1a5794be6b18b
Ahad Shaikh <ashaikh@archersystems.com>
2026-08-14 18:59:14 +0530

    Added xUnit test project

---
+++ JenkinsWebApi.Tests/EmployeeServiceTests.cs
@@ -0,0 +1,28 @@
+﻿using JenkinsWebApi.Models;
+using JenkinsWebApi.Services;
+
+namespace JenkinsWebApi.Tests;
+
+public class EmployeeServiceTests
+{
+    [Fact]
+    public void Create_Should_Add_New_Employee()
+    {
+        // Arrange
+        var service = new EmployeeService();
+
+        var employee = new Employee
+        {
+            Name = "Ahad",
+            Department = "DevOps",
+            Salary = 50000m
+        };
+
+        // Act
+        var result = service.Create(employee);
+
+        // Assert
+        Assert.True(result.Id > 0);
+        Assert.Equal("Ahad", result.Name);
+    }
+}
